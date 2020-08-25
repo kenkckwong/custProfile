52c528db80e088d4e906b66275068cd689b8db59
kenkckwong <66596383+kenkckwong@users.noreply.github.com>
2020-08-25 11:48:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.get('/api-endpoint', function(request, response) {
             profileContent: {title: "Stanley Ng", verified: "Non-verified", profileIcon: "https://devshift.biz/wp-content/uploads/2017/04/profile-icon-png-898.png"},
             customTag: [{"id":"1", "title":"Blacklisted" },
                         {"id":"2", "title":"ID Verified" }],
+            basicInformation:{"firstTitle":"Date of Birth","firstContent":"3yr 10mo","secondTitle":"Doc Type/ID","secondContent":"HKID Y1234567(A)"},
           
         };
     
